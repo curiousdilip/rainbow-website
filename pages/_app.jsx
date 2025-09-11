c4ba0c2631634c53a891a55f80bd96d0e561d0c2
Dilip Kumar Maurya <curiousdilip@gmail.com>
2025-09-11 05:56:07 +0000

    assets position changed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,38 +1,30 @@
-import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="/assets/css/preloader.css" />
-        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="/assets/css/slick.css" />
-        <link rel="stylesheet" href="/assets/css/metisMenu.css" />
-        <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
-        <link rel="stylesheet" href="/assets/css/animate.min.css" />
-        <link rel="stylesheet" href="/assets/css/jquery.fancybox.min.css" />
-        <link rel="stylesheet" href="/assets/css/all.min.css" />
-        <link rel="stylesheet" href="/assets/css/flaticon.css" />
-        <link rel="stylesheet" href="/assets/css/nice-select.css" />
-        <link rel="stylesheet" href="/assets/css/default.css" />
-        <link rel="stylesheet" href="/assets/css/style.css" />
-      </Head>
 
-      <Script src="/assets/js/vendor/jquery.js" />
-      <Script src="/assets/js/bootstrap.min.js"  />
-      <Script src="/assets/js/waypoint.js"  />
-      <Script src="/assets/js/metisMenu.min.js" />
-      <Script src="/assets/js/slick.min.js" />
-      <Script src="/assets/js/jquery.fancybox.min.js" />
-      <Script src="/assets/js/isotope.pkgd.min.js" />
-      <Script src="/assets/js/owl.carousel.min.js" />
-      <Script src="/assets/js/ajax-form.js" />
-      <Script src="/assets/js/jquery.nice-select.min.js" />
-      <Script src="/assets/js/wow.min.js" />
-      <Script src="/assets/js/imagesloaded.pkgd.min.js" />
-      <Script src="/assets/js/jquery.counterup.min.js" />
-      <Script src="/assets/js/main.js" />
+      <Script src="/assets/js/vendor/jquery.js" strategy="beforeInteractive" />
+
+    
+      <Script src="/assets/js/bootstrap.min.js" strategy="afterInteractive" />
+
+
+      <Script src="/assets/js/waypoint.js" strategy="afterInteractive" />
+      <Script src="/assets/js/metisMenu.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/slick.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/jquery.fancybox.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/isotope.pkgd.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/owl.carousel.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/ajax-form.js" strategy="afterInteractive" />
+      <Script src="/assets/js/jquery.nice-select.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/wow.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/imagesloaded.pkgd.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/jquery.counterup.min.js" strategy="afterInteractive" />
+
+   
+      <Script src="/assets/js/main.js" strategy="afterInteractive" />
+
       <Component {...pageProps} />
     </>
   );
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+    
+      <Head>
+        <link rel="stylesheet" href="/assets/css/preloader.css" />
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/slick.css" />
+        <link rel="stylesheet" href="/assets/css/metisMenu.css" />
+        <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="/assets/css/animate.min.css" />
+        <link rel="stylesheet" href="/assets/css/jquery.fancybox.min.css" />
+        <link rel="stylesheet" href="/assets/css/all.min.css" />
+        <link rel="stylesheet" href="/assets/css/flaticon.css" />
+        <link rel="stylesheet" href="/assets/css/nice-select.css" />
+        <link rel="stylesheet" href="/assets/css/default.css" />
+        <link rel="stylesheet" href="/assets/css/style.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
